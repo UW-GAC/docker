02c45874a8556875d4c40c36ad3c6cc54e04ff95
roy <rollinforall@gmail.com>
2018-04-10 12:08:01 -0700

    cleanup unused functions

--- analysis_pipeline.py
+++ analysis_pipeline.py
@@ -15,22 +15,6 @@ version='1.0'
 msgErrPrefix='>>> Error (' + os.path.basename(__file__) +')'
 msgInfoPrefix='>>> Info (' + os.path.basename(__file__) +')'
 debugPrefix='>>> Debug (' + os.path.basename(__file__) +')'
-# def logger class
-
-class Logger(object):
-    def __init__(self, logfile):
-        self.terminal = sys.stdout
-        self.log = open(logfile, "a")
-
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
-
-    def flush(self):
-        #this flush method is needed for python 3 compatibility.
-        #this handles the flush command by doing nothing.
-        #you might want to specify some extra behavior here.
-        pass
 
 # def functions
 def pInfo(msg):
